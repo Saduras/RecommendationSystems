6707d6e52c60b3bb6b91926a04e3ce937711526a
David Speck <david.speck@wooga.net>
2018-02-04 16:18:20 +0100

    Map parsed lastfm data to matricies and label map

--- fetch_lastfm.py
+++ fetch_lastfm.py
@@ -1,4 +1,8 @@
 import zipfile
+
+import numpy as np
+import scipy.sparse as sp
+
 from lightfm.datasets import _common
 
 def _read_raw_data(path):
@@ -33,6 +37,22 @@ def _parse_users(raw_users):
         uid, aid, weight = [int(x) for x in line.split('\t')]
         yield uid, aid, weight
 
+def _get_dimensions(artists, users):
+
+    uids = set()
+    aids = set()
+
+    for uid, _, _ in users:
+        uids.add(uid)
+
+    for aid, _, _, _ in artists:
+        aids.add(aid)
+
+    rows = max(uids) + 1
+    cols = max(aids) + 1
+
+    return rows, cols
+
 def fetch_lastfm():
     """
     Fetch the 'hetrec 2011 lastfm 2k data set <https://grouplens.org/datasets/hetrec-2011/>' 
@@ -42,13 +62,28 @@ def fetch_lastfm():
 
     (raw_artists, raw_users) = _read_raw_data(zip_path)
 
-    artists = _parse_artists(raw_artists)
-    users = _parse_users(raw_users)
+    rows, cols = _get_dimensions(_parse_artists(raw_artists), 
+                                _parse_users(raw_users))
+
+    testSize = int(rows * 0.2)
+    trainSize = rows - testSize
+
+    artist_map = np.empty(cols, dtype=np.object)
+    for aid, name, _, _ in _parse_artists(raw_artists):
+        artist_map[aid] = name
+
+    train_mat = sp.lil_matrix((trainSize, cols), dtype=np.int32)
+    test_mat = sp.lil_matrix((testSize, cols), dtype=np.int32)
+    for (uid, aid, weight) in _parse_users(raw_users):
+        if uid < trainSize:
+            train_mat[uid, aid] = weight
+        else:
+            test_mat[uid - trainSize, aid] = weight
 
     
-    # print(users)
-    # for user in users:
-    #     user['artistID'] = artists
-
-
-fetch_lastfm()
+    data = {
+        'train' : train_mat.tocoo(),
+        'test' : test_mat.tocoo(),
+        'item_labels' : artist_map,
+    }
+    return data
